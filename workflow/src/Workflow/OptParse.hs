f62825c26f1b330281df81b90bd641b3168a8058
Nickske666 <nick.van.den.broeck@hotmail.com>
2017-05-02 12:48:46 -0400

    Added the parsing of input.

--- workflow/src/Workflow/OptParse.hs
+++ workflow/src/Workflow/OptParse.hs
@@ -1,25 +1,97 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+
 module Workflow.OptParse
     ( module Workflow.OptParse
     , module Workflow.OptParse.Types
     ) where
 
-import Introduction
-
+import Data.Configurator
+import Import hiding (lookup)
 import Options.Applicative
-
+import System.Environment
 import Workflow.OptParse.Types
 
 getInstructions :: IO Instructions
 getInstructions = do
     (cmd, flags) <- getArguments
-    config <- getConfiguration cmd flags
-    combineToInstructions cmd flags config
+    dispatch <- getDispatch cmd flags
+    pure (dispatch, getSettings flags)
 
-combineToInstructions :: Command -> Flags -> Configuration -> IO Instructions
-combineToInstructions Command Flags Configuration = pure (Dispatch, Settings)
+getSettings :: Flags -> Settings
+getSettings _ = Settings
 
-getConfiguration :: Command -> Flags -> IO Configuration
-getConfiguration _ _ = pure Configuration
+getDispatch :: Command -> Flags -> IO Dispatch
+getDispatch cmd flags = do
+    config <- getConfig cmd flags
+    getDispatchFromConfig cmd config
+
+getDispatchFromConfig :: Command -> Configuration -> IO Dispatch
+getDispatchFromConfig CommandWaiting {..} Configuration {..} = do
+    configPath <- parseAbsDir workDirConfig
+    pure $ DispatchWaiting configPath shouldPrintConfig
+getDispatchFromConfig CommandNext {..} Configuration {..} = do
+    configPath <- parseAbsDir workDirConfig
+    pure $ DispatchNext configPath shouldPrintConfig
+
+getConfig :: Command -> Flags -> IO Configuration
+getConfig CommandWaiting {..} _ =
+    getConfigFromInput workDirCommand configFile shouldPrint
+getConfig CommandNext {..} _ =
+    getConfigFromInput workDirCommand configFile shouldPrint
+
+getConfigFromInput :: Maybe FilePath
+                   -> Maybe FilePath
+                   -> ShouldPrint
+                   -> IO Configuration
+getConfigFromInput workDir configFile _ = do
+    configPath <-
+        case configFile of
+            Nothing -> defaultConfigFile
+            Just path -> resolveFile' path
+    dirPath <- getDirPathFromConfigPath configPath
+    shouldPrintConfig <- getShouldPrintPathFromConfigPath configPath
+    workDirConfig <-
+        case workDir of
+            Nothing -> pure dirPath
+            Just directoryPath -> parseAbsDir directoryPath
+    pure $ Configuration (fromAbsDir workDirConfig) shouldPrintConfig
+
+getDirPathFromConfigPath :: Path Abs File -> IO (Path Abs Dir)
+getDirPathFromConfigPath confPath = do
+    config <- load [Optional $ toFilePath confPath]
+    dirPathString <- lookup config "path"
+    formatDirPath dirPathString
+
+getShouldPrintPathFromConfigPath :: Path Abs File -> IO ShouldPrint
+getShouldPrintPathFromConfigPath confPath = do
+    config <- load [Optional $ toFilePath confPath]
+    shouldPrintConfig <- lookup config "shouldPrint"
+    case shouldPrintConfig of
+        Nothing -> pure defaultShouldPrint
+        Just shouldPrint -> pure shouldPrint
+
+formatDirPath :: Maybe String -> IO (Path Abs Dir)
+formatDirPath Nothing = do
+    home <- getHomeDir
+    resolveDir home "workflow"
+formatDirPath (Just dirPathString) =
+    case dirPathString of
+        '~':_ -> do
+            home <- getHomeDir
+            dirPath <- resolveDir home $ drop 2 dirPathString
+            print dirPath
+            pure dirPath
+        _ -> do
+            currentDir <- getCurrentDir
+            resolveDir currentDir dirPathString
+
+defaultConfigFile :: IO (Path Abs File)
+defaultConfigFile = do
+    homeDir <- getHomeDir
+    resolveFile homeDir ".wfrc"
 
 getArguments :: IO Arguments
 getArguments = do
@@ -30,14 +102,16 @@ getArguments = do
 runArgumentsParser :: [String] -> ParserResult Arguments
 runArgumentsParser = execParserPure prefs_ argParser
   where
-    prefs_ = ParserPrefs
-      { prefMultiSuffix = ""
-      , prefDisambiguate = True
-      , prefShowHelpOnError = True
-      , prefShowHelpOnEmpty = True
-      , prefBacktrack = True
-      , prefColumns = 80
-      }
+    prefs_ =
+        ParserPrefs
+        { prefMultiSuffix = ""
+        , prefDisambiguate = True
+        , prefShowHelpOnError = True
+        , prefShowHelpOnEmpty = True
+        , prefBacktrack = True
+        , prefColumns = 80
+        }
+
 argParser :: ParserInfo Arguments
 argParser = info (helper <*> parseArgs) help_
   where
@@ -48,16 +122,53 @@ parseArgs :: Parser Arguments
 parseArgs = (,) <$> parseCommand <*> parseFlags
 
 parseCommand :: Parser Command
-parseCommand = hsubparser $ mconcat
-    [ command "command" parseCommandCommand
-    ]
+parseCommand =
+    hsubparser $
+    mconcat
+        [command "waiting" parseCommandWaiting, command "next" parseCommandNext]
 
-parseCommandCommand :: ParserInfo Command
-parseCommandCommand = info parser modifier
+parseCommandWaiting :: ParserInfo Command
+parseCommandWaiting = info commandParser modifier
   where
-    parser = pure Command
-    modifier = fullDesc
-            <> progDesc "Command example."
+    modifier = fullDesc <> progDesc "Print a list of the \"waiting\" tasks"
+
+parseCommandNext :: ParserInfo Command
+parseCommandNext = info commandParser modifier
+  where
+    modifier =
+        fullDesc <>
+        progDesc
+            "Print the list of \"next\" tasks as well as the files which don't have a \"next\" action"
+
+commandParser :: Parser Command
+commandParser =
+    CommandWaiting <$>
+    option
+        (Just <$> str)
+        (mconcat
+             [ long "config-file"
+             , help "Give the path to an altenative config file"
+             , value Nothing
+             , metavar "FILEPATH"
+             ]) <*>
+    option
+        (Just <$> str)
+        (mconcat
+             [ long "workflow-dir"
+             , help "Give the path to the workflow directory to be used"
+             , value Nothing
+             , metavar "FILEPATH"
+             ]) <*>
+    option
+        (maybeReader getShouldPrint)
+        (mconcat
+             [ long "should-print"
+             , help
+                   "This describes whether error messages should be handled as errors (\"error\"), warnings (\"warning\") or ignored (\"nothing\")."
+             , showDefault
+             , value defaultShouldPrint
+             , metavar "shouldPrint"
+             ])
 
 parseFlags :: Parser Flags
 parseFlags = pure Flags
