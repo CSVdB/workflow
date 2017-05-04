1d536ceda8c4894db0bfce77f90ce9c0184653fc
Nickske666 <nick.van.den.broeck@hotmail.com>
2017-05-04 11:58:59 -0400

    Added feature "waiting".

--- workflow/src/Workflow/Utils.hs
+++ workflow/src/Workflow/Utils.hs
@@ -18,8 +18,8 @@ import System.FilePath.Posix
 import Text.PrettyPrint.Boxes
 import Workflow.OptParse
 
-formatStringAsTables :: [[String]] -> String
-formatStringAsTables list =
+formatStringAsTable :: [[String]] -> String
+formatStringAsTable list =
     let boxes = transpose $ fmap text <$> list
         table = hsep 1 center1 $ fmap (vcat left) boxes
     in render table
@@ -32,7 +32,7 @@ getHeadingsFromDir workDir _ = do
     textList <- mapM (readFileAndRememberPath workDir) files
     let (errorMessages, listHeadings) =
             partitionEithers $ fmap getHeadingsFromFile textList
-    pure (concat listHeadings, concat errorMessages)
+    pure (concat listHeadings, unlines errorMessages)
 
 getFilesFromDir :: Path Abs Dir -> IO [Path Abs File]
 getFilesFromDir workPath = do
