9572560d8c60d5877e7fedc00fdd51c7280c5a42
Nickske666 <nick.van.den.broeck@hotmail.com>
2017-05-04 11:58:59 -0400

    Added feature "waiting".

--- workflow/src/Workflow.hs
+++ workflow/src/Workflow.hs
@@ -1,6 +1,15 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Workflow where
 
-import Introduction
+import Import
+import Workflow.OptParse
+import Workflow.Waiting
 
 workflow :: IO ()
-workflow = putStrLn "hi"
+workflow = do
+    (disp, sett) <- getInstructions
+    execute disp sett
+
+execute :: Dispatch -> Settings -> IO ()
+execute DispatchWaiting {..} = waiting workDir shouldPrintDispatch
